e8fab9c6e703a7fac5d72c2da7e6c7171663921e
Junkdog-04 <thejokerslayme@hotmail.com>
2026-07-17 22:44:22 -0600

    CREATE: make all functions to solve the first module

--- main.js
+++ main.js
@@ -38,3 +38,62 @@ function average(){
   let result = document.getElementById('result')
   return result.innerHTML = `Your average grade is ` + averageGrade
 }
+
+function squaremeters(){
+  let totalmeters = Number(document.getElementById('meters').value)
+  let coveredmeters = Number(document.getElementById('covered').value)
+  let percent = 100
+  let percentcovered =  (coveredmeters /  totalmeters ) * percent
+  let uncovered = percent - percentcovered
+  let percentresult = document.getElementById('percentresult')
+  let uncoveredresult = document.getElementById('uncover')
+  
+  percentresult.innerHTML = `Percent covered: ` + percentcovered + `%`
+  uncoveredresult.innerHTML = `Percent uncovered: ` + uncovered + `%`
+}
+
+function discount(){
+  let totalpurchase = Number(document.getElementById('total').value)
+  let percentdiscount = 15
+  let percent = 100
+  let originalprice = (totalpurchase * percentdiscount) / percent
+  let pricewithdiscount = totalpurchase - originalprice
+
+  let totaldiscount = document.getElementById('totaldiscount')
+  return totaldiscount.innerHTML = `Your total with discount is  ` + pricewithdiscount
+}
+
+function totalgener(){
+  let totalmen = Number(document.getElementById('totalmen').value)
+  let totalwomen = Number(document.getElementById('totalwomen').value)
+  let percent = 100
+  let totalpeople = totalmen + totalwomen
+  let percentmen = (totalmen / totalpeople) * percent
+  let percentwomen = (totalwomen / totalpeople) * percent
+
+  let resultmen = document.getElementById('resultmen')
+  let resultwomen = document.getElementById('resultwomen')
+
+  resultmen.innerHTML = `Percent men: ` + percentmen + `%`
+  resultwomen.innerHTML = `Percent women: ` + percentwomen + `%`
+} 
+
+function operations(){
+  let firstnumber = Number(document.getElementById('numberone').value)
+  let secondnumber = Number(document.getElementById('numbertwo').value)
+
+  let add = firstnumber + secondnumber
+  let subtract = firstnumber - secondnumber
+  let multiply = firstnumber * secondnumber
+  let division = firstnumber / secondnumber
+
+  let resultadd = document.getElementById('add')
+  let resultsubtract = document.getElementById('subtract')
+  let resultmultiply = document.getElementById('multiply')
+  let resultdivision = document.getElementById('division')
+
+  resultadd.innerHTML = `Your add is: ` + add
+  resultsubtract.innerHTML = `Your subtract is : ` + subtract
+  resultmultiply.innerHTML = `Your multiply is : ` + multiply
+  resultdivision.innerHTML = `Your division is : ` + division
+}
